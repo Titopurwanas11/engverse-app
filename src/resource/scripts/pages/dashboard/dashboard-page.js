f1b49eeef6d042aeb1a6982388d2e1d25429b854
FanidiyaTasya <fanidiyatasya@gmail.com>
2025-05-23 13:03:13 +0700

    refactor: move header to its own component and fix layout styles

--- src/resource/scripts/pages/dashboard/dashboard-page.js
+++ src/resource/scripts/pages/dashboard/dashboard-page.js
@@ -2,7 +2,8 @@ export default class DashboardPage {
   async render() {
     return `
       <section class="container">
-        <h1>Dashboard Page</h1>
+        <h1 class="text-2xl font-bold">Selamat Datang Tasyyaaa</h1>
+        <p>Dashboard Page</p>
       </section>
     `;
   }
